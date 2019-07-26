def1c847e98f23d061a5994b8c3dbb812a7e5194
fumeng <405181821@qq.com>
2019-07-26 17:07:00 +0800

    动态障碍怎么搞，怎么搞啊？

---
+++ Assets/FNavigation/UnityUseLayer/UnityObstacle.cs
@@ -0,0 +1,32 @@
+﻿using org.critterai.nav;
+using org.critterai.nav.u3d;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityObstacle : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    public ScriptableObject mesh;
+
+    public void ReBake()
+    {
+        /*
+        TileBuildData BuildData 
+
+
+        INavmeshData theNavMesh = (INavmeshData)mesh;
+        //NavmeshTile theTile  = theNavMesh.GetNavmesh().GetTile(1,1,1);
+        NavmeshBuildInfo configUse = new NavmeshBuildInfo();
+
+        theNavMesh.Load(,theNavMesh.BuildInfo);
+        NavmeshTileData[] tiles = null;
+         */
+    }
+   
+}
